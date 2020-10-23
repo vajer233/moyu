97104418752684d6f4ac17439bf556f344f54371
xinglei233 <59872475+xinglei233@users.noreply.github.com>
2020-10-23 22:55:30 +0800

    Add files via upload

--- puqingc.py
+++ puqingc.py
@@ -4,14 +4,15 @@ import random
 
 
 def random_sentence():
+    int = random.randint(1,3)
     a = "起来！站到十二点！"
     b = "都几点了还讲话！"
     c = "下来！去写200字反思再睡！"
-    if random.randint(1, 3) == 1:
+    if int == 1:
         return a
-    if random.randint(1, 3) == 2:
+    elif int == 2:
         return b
-    if random.randint(1, 3) == 3:
+    elif int == 3:
         return c
 
 
