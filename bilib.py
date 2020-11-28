3c1753293c3baaa6236e8eea444dd97de42d3f84
xinglei233 <59872475+xinglei233@users.noreply.github.com>
2020-11-28 19:26:32 +0800

    Update bilib.py

--- bilib.py
+++ bilib.py
@@ -1,21 +1,21 @@
-import requests
-import json
+# import requests
+# import json
 
-def dingTalk():
-    headers={
-        "Content-Type": "application/json"
-            }
-    data={
-            "actionCard": {
-                "title":"哔哩哔哩英文人声三日榜",
-                "text": "![a.jpg](https://i.loli.net/2020/08/06/j1d7S8kamNhvBor.jpg) 哔哩哔哩英文人声三日榜 \n放松一下，听首歌吧～",
-                "singleTitle":"查看网页",
-                "hideAvatar":"0",
-                "singleURL": 'https://m.bilibili.com/audio/am30474',
-            },
-        "msgtype": "actionCard",
-          }
-    json_data=json.dumps(data)
-    requests.post(url='https://oapi.dingtalk.com/robot/send?access_token=94d6f3103ea61db64868ddc6ef02778f1ae2adf8334780db2860efa2a321c1cd',data=json_data,headers=headers)
-dingTalk()
+# def dingTalk():
+#     headers={
+#         "Content-Type": "application/json"
+#             }
+#     data={
+#             "actionCard": {
+#                 "title":"哔哩哔哩英文人声三日榜",
+#                 "text": "![a.jpg](https://i.loli.net/2020/08/06/j1d7S8kamNhvBor.jpg) 哔哩哔哩英文人声三日榜 \n放松一下，听首歌吧～",
+#                 "singleTitle":"查看网页",
+#                 "hideAvatar":"0",
+#                 "singleURL": 'https://m.bilibili.com/audio/am30474',
+#             },
+#         "msgtype": "actionCard",
+#           }
+#     json_data=json.dumps(data)
+#     requests.post(url='https://oapi.dingtalk.com/robot/send?access_token=94d6f3103ea61db64868ddc6ef02778f1ae2adf8334780db2860efa2a321c1cd',data=json_data,headers=headers)
+# dingTalk()
 
